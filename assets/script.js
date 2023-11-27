879da1e3f449a6ed7f71acede37b2d0bb721726e
Clive Makongo <clivemakongo@gmail.com>
2023-11-27 10:12:12 +0000

    removed started folder

--- assets/script.js
+++ assets/script.js
@@ -90,46 +90,86 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-let passOpt = {};
+  let passOpt = {};
 
-    let length = prompt("How long should the password be?");
-    if (length > 7 && length < 129) {
-      let lowercase = confirm("Do you want lowercase letters?");
-      let uppercase = confirm("Do you want uppercase letters");
-      let numbers = confirm("Do you want numbers?");
-      let specChar = confirm("Do you want special characters?");
+  let length = prompt("How long should the password be?");
+  if (length > 7 && length < 129) {
+    let lowercase = confirm("Do you want lowercase letters?");
+    let uppercase = confirm("Do you want uppercase letters");
+    let numbers = confirm("Do you want numbers?");
+    let specChar = confirm("Do you want special characters?");
 
-      passOpt = {
-        length: length,
-        lowercase: lowercase,
-        uppercase: uppercase,
-        numbers: numbers,
-        specChar: specChar,
-      
-      };
-
-      console.log(passOpt);
-
-    } else {
-      alert("Your password must be between 8 and 128 characters long");
-      getPasswordOptions();
-    }
-
-    
+    passOpt = {
+      length: length,
+      lowercase: lowercase,
+      uppercase: uppercase,
+      numbers: numbers,
+      specChar: specChar,
+    };
+  } else {
+    alert("Your password must be between 8 and 128 characters long");
+    getPasswordOptions();
+  }
 
   return passOpt;
-
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  Math.floor(Math.random * arr.length);
+  let id = Math.floor(Math.random() * arr.length);
+  console.log(id + ": ID");
+  return id;
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(
+  specialCharacters,
+  lowerCasedCharacters,
+  upperCasedCharacters,
+  numericCharacters
+) {
   let options = getPasswordOptions();
-  
+  let pass = "";
+  console.log(pass, options);
+
+  //Turn off Choices that were not selected
+  for (i = 0; i < options.length; i++) {
+    let whichType = [
+      specialCharacters,
+      numericCharacters,
+      upperCasedCharacters,
+      lowerCasedCharacters,
+    ];
+
+    let discard;
+
+    if (!options.lowercase) {
+      discard = whichType.splice(whichType.indexOf(lowerCasedCharacters), 1);
+      console.log(whichType, discard, 'lower');
+    }
+
+    if (!options.specChar) {
+      discard = whichType.splice(whichType.indexOf(specialCharacters), 1);
+      console.log(whichType, discard, 'spec');
+    }
+    
+    if (!options.numbers) {
+      discard = whichType.splice(whichType.indexOf(numericCharacters), 1);
+      console.log(whichType, discard, 'num');
+    }
+
+    if (!options.uppercase) {
+      discard = whichType.splice(whichType.indexOf(upperCasedCharacters), 1);
+      console.log(whichType, discard, 'upper');
+    }
+
+    let choice = getRandom(whichType);
+    let actChoice = getRandom(whichType[choice]);
+    pass = pass.concat(whichType[choice][actChoice]);
+    console.log(pass, whichType[choice], whichType, options);
+  }
+
+  return pass;
 }
 
 // Get references to the #generate element
@@ -137,7 +177,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(
+    specialCharacters,
+    lowerCasedCharacters,
+    upperCasedCharacters,
+    numericCharacters
+  );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
